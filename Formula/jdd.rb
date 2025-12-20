fe2083f1179191f43b9e914321f39dfb0a60d078
Hunter Gatewood <hgatewood@gmail.com>
2025-12-20 12:23:02 -0600

    Bump jdd

--- Formula/jdd.rb
+++ Formula/jdd.rb
@@ -1,8 +1,8 @@
 class Jdd < Formula
     desc "Navigate successive versions of a JSON object"
     homepage "https://github.com/hcgatewood/jdd"
-    url "https://github.com/hcgatewood/jdd/archive/refs/tags/v0.12.0.tar.gz"
-    sha256 "90eb76e4b132ab9713879df856429f2de229ff83e5ab03905b320f36495f8000"
+    url "https://github.com/hcgatewood/jdd/archive/refs/tags/v0.13.0.tar.gz"
+    sha256 "495ab56195db24951469d452e6f51ae5627fddff97bc066007247ae832918d1a"
 
     depends_on "fswatch"
     depends_on "fx"
